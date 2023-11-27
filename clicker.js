dd07e54e283cec56b865eada393bb399cf58da55
malpig <115734177+malpig@users.noreply.github.com>
2023-11-27 16:16:23 -0500

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,34 +1,21 @@
-if (ClickerCookie === undefined) var ClickerCookie = {};
-ClickerCookie.name = 'Auto Clicker for Cookies';
-ClickerCookie.version = '1.00';
-ClickerCookie.GameVersion = '2.052';
-ClickerCookie.launch = function ()
-{
-    ClickerCookie.isLoaded = 1;
-    var autoBigCookie = setInterval(function ()
-    {
-        trigger = document.createEvent('HTMLEvents');
-        trigger.initEvent("click", true, true);
-        bigCookie.dispatchEvent(trigger);
-    }, 25);
-    var autoPassCookie = setInterval(function ()
-    {
-        for (var h in Game.shimmers)
-        {
-            if (Game.shimmers[h].type == "golden" || Game.shimmers[h].type == "reindeer")
-            {
-                Game.shimmers[h].pop();
-            }
-        }
-    }, 25);
-}
 
-if (!ClickerCookie.isLoaded)
-{
-    ClickerCookie.launch();
-    console.log(" Mod \"Clicker Cookie\" added.");
-}
-else
-{
-    console.log("Clicker Cookie Already Loaded");
-}
+trigger = document.createEvent('HTMLEvents');
+trigger.initEvent("click", true, true);
+var car = setInterval(function()
+            {
+              bigCookie.dispatchEvent(trigger);
+              for (var h in Game.shimmers)
+              {
+                if(Game.shimmers[h].type != "Wraith cookie")
+                   {
+                      Game.shimmers[h].pop();
+                   }
+              }
+            },1);
+var tc = setInterval(function()
+                     {
+                          if (Game.TickerEffect && Game.TickerEffect.type=='fortune') 
+                          {
+                                  Game.tickerL.click();
+                          }
+                     },500);
