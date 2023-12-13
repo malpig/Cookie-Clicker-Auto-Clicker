405834aff4583d8407a59dbf5b57a22b2c9da4c8
malpig <115734177+malpig@users.noreply.github.com>
2023-12-13 16:11:14 -0500

    Create Inspect2.js

---
+++ Inspect2.js
@@ -0,0 +1,9 @@
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+      eval(prompt("Eval:"));
+      script.src="https://cdn.jsdelivr.net/npm/eruda";
+  }
+  script.src="https://cdn.jsdelivr.net/npm/eruda";
+})
+script.src="https://cdn.jsdelivr.net/npm/eruda";
