683c54a5d3e72e70137ee6d19e3f226c86b756d2
malpig <115734177+malpig@users.noreply.github.com>
2023-12-13 16:13:13 -0500

    Update Inspect2.js

--- Inspect2.js
+++ Inspect2.js
@@ -2,8 +2,6 @@
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
       eval(prompt("Eval:"));
-      script.src="https://cdn.jsdelivr.net/npm/eruda";
+      var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); }
   }
-  script.src="https://cdn.jsdelivr.net/npm/eruda";
 })
-script.src="https://cdn.jsdelivr.net/npm/eruda";
